cff12a37da140d7e977911070c5a1b17f2b9f678
Gary Homewood <gary.homewood@gmail.com>
2017-01-23 13:52:42 +0000

    volume set command by platform

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import jsonify
 from flask import session
 
 import os
+import platform
 import subprocess
 import signal
 import config
@@ -20,33 +21,33 @@ def index():
 @app.route("/volume", methods=["POST"])
 def volume():
     content = request.get_json()
-    # set the volume for mac
-    cmd = 'osascript -e \'set volume output volume ' + str(content['volume']) + '\''
-    proc = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, preexec_fn = os.setsid)
+    setVolume(str(content['volume']))
     return 'ok'
 
 @app.route("/play", methods=["POST"])
 def play():
     content = request.get_json()
-
     if 'pid' in session:
         # stop the drone if there is one
         os.killpg(os.getpgid(int(session['pid'])), signal.SIGTERM)
         session.pop('pid', None)
         return 'ok'
-
+    setVolume(str(content['volume']))
     center = str(content['center'])
     wave = str(content['wave'])
-    volume = str(content['volume'])
-
-    # set the volume first
-    setVolumeCmd = 'osascript -e \'set volume output volume ' + volume + '\''
-    subprocess.Popen(setVolumeCmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, preexec_fn = os.setsid)
-
     playNoiseCmd = './noise.sh 59 ' + center + ' ' + wave + ' brown'
     proc = subprocess.Popen(playNoiseCmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, preexec_fn = os.setsid)
-
     # save the drone id so it can be turned off
     pid = os.getpgid(proc.pid)
     session['pid'] = str(pid)
     return 'ok'
+
+def setVolume(volume):
+    setVolumeCmd = ''
+    if platform.system() == 'Darwin':
+        setVolumeCmd = 'osascript -e \'set volume output volume ' + volume + '\''
+    if platform.system() == 'Linux':
+        setVolumeCmd = 'amixer set \'Power Amplifier\' ' + volume + '%'
+    if setVolumeCmd:
+        subprocess.Popen(setVolumeCmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT, preexec_fn = os.setsid)
+    return
